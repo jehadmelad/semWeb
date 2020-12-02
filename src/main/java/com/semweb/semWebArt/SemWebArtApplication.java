1027eaaa118c7c48cdb66be7f2990debea74a6c5
jehadmelad <jd.melad@gmail.com>
2020-12-02 22:31:08 +0100

    qurey data

---
+++ src/main/java/com/semweb/semWebArt/SemWebArtApplication.java
@@ -0,0 +1,13 @@
+package com.semweb.semWebArt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemWebArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemWebArtApplication.class, args);
+	}
+
+}
